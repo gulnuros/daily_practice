93dfda6f7ac27d8c313856e7dc2675f4de1dc651
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2025-05-11 13:14:04 -0400

    Next Greater Element I - Monotonick stack

---
+++ Stack/NextGreaterElementI.java
@@ -0,0 +1,26 @@
+package Stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class NextGreaterElementI {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return res;
+    }
+}
