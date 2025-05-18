3cb7a65876db299029430873498a3d7dec6a26d4
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2025-05-17 23:01:08 -0400

    Optimized solution with O(1) space complexity

--- String/LengthofLastWord.java
+++ String/LengthofLastWord.java
@@ -8,4 +8,20 @@ public class LengthofLastWord {
     }
 }
 
-// Length of Last Word
+// optimized solution
+// class Solution {
+// public int lengthOfLastWord(String s) {
+// int len =0;
+// int i = s.length() - 1;
+// while(i>=0 && s.charAt(i) == ' '){
+// i --;
+// }
+
+// while(i>=0 && s.charAt(i) != ' '){
+// len++;
+// i--;
+// }
+
+// return len;
+// }
+// }
