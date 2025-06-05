19048fc868bde6039da44b1e17c8dd106bf6fffa
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2025-06-04 23:56:35 -0400

    Max IceCream bars

---
+++ LinearSort/MaximumIceCreamBars.java
@@ -0,0 +1,18 @@
+package LinearSort;
+
+import java.util.Arrays;
+
+public class MaximumIceCreamBars {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+
+        int n = costs.length;
+        int icecream = 0;
+        while (icecream < n && costs[icecream] <= coins) {
+            coins -= costs[icecream];
+            icecream += 1;
+        }
+
+        return icecream;
+    }
+}
