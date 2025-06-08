560606b9c842bfde531c6e97ecbe79b0d145906b
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2025-06-08 15:26:21 -0400

    Non-overlapping Intervals

---
+++ Heap/NonOverlappingIntervals.java
@@ -0,0 +1,101 @@
+package Heap;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class NonOverlappingIntervals {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        // [[1,2],[2,3],[3,4],[1,3], [3,4], [1,2]]
+        // [[1,2], [2,3],[3,4]]
+        // minheap
+        // top - 2
+
+        // start 1 >=? minheap 2 good
+        // minheap.pop
+        // add current end 2 add to minheap
+        // start 1 >=? minheap 2 not good
+        // count of return++
+        // minheap.pop
+        // add current end 3 add to minheap
+        // start 2 >=? minheap 3 not good
+        // count of return++
+        // minheap.pop
+        // add current end 3 add to minheap
+        // start 3 >=? minheap 3 good
+        // minheap.pop
+        // add current end 4 add to minheap
+        // start 3 >=? minheap 4 not good
+        // count of return++
+
+        // ignore
+        // [[1,2],[1,3], [2,3],[3,4],]
+
+        // count++
+
+        // [[1,2],[2, 3]]
+
+        // [,[1,11],[1,100], [2,12],[11,22]]
+        // minheap 11 current 1
+        // count++
+        // minheap 11 current 2
+        // count++
+        // minheap 11 current 11
+        //
+
+        // [[-52,31],[-73,-26],[82,97],[-65,-11],[-62,-49],[95,99],[58,95],[-31,49],[66,98],[-63,2],[30,47],[-40,-26]]
+
+        // [-73, -26][-65, -11] [-63, 2][-62, -49][-52, 31][-40, -26][-31, 49][30,
+        // 47][58, 95][66, 98][82, 97][95, 99]
+
+        // -26 ? -65 not good
+        // count++
+
+        // -26 ? -63 not good count++
+
+        // -26 ? -62 not good count ++
+
+        // -26 ? -52 not good count++
+        // -26 ? -40 not good count++
+        // -26 ? -31 not good count++
+        // -26 ? 30 add minheap 47
+        // 47 ? 58 add minheap 95
+        // 95 ? 66 not good count++
+        // 95 ? 82 not good count++
+        // 95 ? 95
+
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]);
+            } else {
+                return Integer.compare(a[1], b[1]);
+            }
+        });
+
+        // Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        // add first end time
+        minHeap.add(intervals[0][1]); // 2
+        int count = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            int currentStartTime = intervals[i][0]; // 1
+            int currentEndTime = intervals[i][1]; // 1
+            int lastEndTime = minHeap.peek();
+            if (currentStartTime < lastEndTime) {
+                count++;
+                if (currentEndTime < lastEndTime) {
+                    // Current one ends earlier — better to keep
+                    minHeap.poll(); // remove the worse one
+                    minHeap.add(currentEndTime); // keep current
+                }
+            } else {
+                // good
+                minHeap.poll();
+                minHeap.add(intervals[i][1]); // current ends
+            }
+
+        }
+        return count;
+
+    }
+}
