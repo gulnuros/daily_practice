f53eb5c1c963a703ee955503656515d85cc6dda7
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2025-06-25 23:01:49 -0400

    Symmetric Tree

---
+++ BinaryTree/SymmetricTree.java
@@ -0,0 +1,34 @@
+package BinaryTree;
+
+public class SymmetricTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+
+    public boolean isMirror(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null)
+            return true;
+        if (node1 == null || node2 == null)
+            return false;
+        return ((node1.val == node2.val) &&
+                isMirror(node1.right, node2.left) &&
+                isMirror(node1.left, node2.right));
+    }
+}
